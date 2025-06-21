b6fe2065390d9b1ee888700ae05ec6fea0fc1304
pedrohv@ciandt.com <pedrohv@ciandt.com>
2025-06-21 14:55:21 -0300

    chore(jest): update jest.config.ts with coverage settings
    - Remove obsolete comment about Next.js app path.
    - Update moduleNameMapper formatting.
    - Add collectCoverage, collectCoverageFrom, and coverageDirectory options to enable coverage collection.
    - Export jest config using createJestConfig(config) for async Next.js config loading.

--- jest.config.ts
+++ jest.config.ts
@@ -2,18 +2,25 @@ import type { Config } from 'jest'
 import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 })
 
-// Add any custom config to be passed to Jest
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-  moduleNameMapper: { '^@/components/(.*)$': '<rootDir>/components/$1', },
-  // Add more setup options before each test is run
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+  },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}', // Inclui todos os arquivos da pasta src
+    '!**/node_modules/**', // Exclui node_modules
+    '!**/.next/**', // Exclui a pasta .next
+    '!**/out/**', // Exclui pasta de build caso exista
+    '!**/*.d.ts', // Exclui arquivos de definição de tipo
+  ],
+  coverageDirectory: 'coverage', // pasta onde o report de coverage será gerado
 }
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config)
